07aa944dbf9c13aba2db45b16caa2c0d34953e34
Laurent LIU <ru0f3ng@gmail.com>
2017-03-17 17:58:14 +0100

    Retro Calculator finished

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -11,7 +11,21 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
     var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    var currentOperation = Operation.Empty
+    var leftValueString = ""
+    var rightValueString = ""
+    var result = ""
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,23 +39,97 @@ class ViewController: UIViewController {
         } catch let err as NSError {
             print(err.debugDescription)
         }
+        
+        outputLabel.text = "0"
     }
     
-    @IBAction func numberPressed(sender: UIButton)
-    {
+    @IBAction func numberPressed(sender: UIButton) {
         playSound()
+        
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
     }
     
-    func playSound()
-    {
-        if btnSound.isPlaying
-        {
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubstractPressed(sender: AnyObject) {
+        processOperation(operation: .Substract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
             btnSound.stop()
         }
         btnSound.play()
     }
-
-
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            
+            if runningNumber != "" {
+                rightValueString = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValueString)! * Double(rightValueString)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValueString)! / Double(rightValueString)!)"
+                } else if currentOperation == Operation.Substract {
+                    result = "\(Double(leftValueString)! - Double(rightValueString)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValueString)! + Double(rightValueString)!)"
+                }
+                
+                leftValueString = result
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        } else {
+            leftValueString = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
